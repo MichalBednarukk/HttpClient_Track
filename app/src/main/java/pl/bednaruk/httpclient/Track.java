aaa913b51eec720bd7f3151038d407d82e3f3983
Michał <mbednaruk12@gmail.com>
2021-05-17 13:05:43 +0200

    track model

---
+++ app/src/main/java/pl/bednaruk/httpclient/Track.java
@@ -0,0 +1,113 @@
+package pl.bednaruk.httpclient;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Track {
+
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("username")
+    @Expose
+    private String username;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("body")
+    @Expose
+    private String body;
+
+    @SerializedName("author")
+    @Expose
+    private String author;
+    @SerializedName("chordApps")
+    @Expose
+    private List<ChordApp> chordApps = null;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public List<ChordApp> getChordApps() {
+        return chordApps;
+    }
+
+    public void setChordApps(List<ChordApp> chordApps) {
+        this.chordApps = chordApps;
+    }
+    
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(Track.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
+        sb.append("id");
+        sb.append('=');
+        sb.append(((this.id == null)?"<null>":this.id));
+        sb.append(',');
+        sb.append("username");
+        sb.append('=');
+        sb.append(((this.username == null)?"<null>":this.username));
+        sb.append(',');
+        sb.append("name");
+        sb.append('=');
+        sb.append(((this.name == null)?"<null>":this.name));
+        sb.append(',');
+        sb.append("body");
+        sb.append('=');
+        sb.append(((this.body == null)?"<null>":this.body));
+        sb.append(',');
+        sb.append("author");
+        sb.append('=');
+        sb.append(((this.author == null)?"<null>":this.author));
+        sb.append(',');
+        sb.append("chordApps");
+        sb.append('=');
+        sb.append(((this.chordApps == null)?"<null>":this.chordApps));
+        sb.append(',');
+        if (sb.charAt((sb.length()- 1)) == ',') {
+            sb.setCharAt((sb.length()- 1), ']');
+        } else {
+            sb.append(']');
+        }
+        return sb.toString();
+    }
+}
