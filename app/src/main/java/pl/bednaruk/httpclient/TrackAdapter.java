f610545f97beabc1f62ed6760643e08d83bf4a4a
Michał <mbednaruk12@gmail.com>
2021-05-17 13:09:18 +0200

    Adapter of tracks for recyclerView

---
+++ app/src/main/java/pl/bednaruk/httpclient/TrackAdapter.java
@@ -0,0 +1,62 @@
+package pl.bednaruk.httpclient;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class TrackAdapter extends RecyclerView.Adapter<TrackAdapter.ViewHolder> {
+    private final List<Track> trackList;
+    Context context;
+
+    public interface ItemClicked {
+        void OnItemClicked(int id);
+
+    }
+    public TrackAdapter(Context context, List<Track> trackList) {
+
+        this.trackList = trackList;
+        this.context = context;
+    }
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tvName, tvChord, tvAuthor;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvName = itemView.findViewById(R.id.tvName);
+            tvChord = itemView.findViewById(R.id.tvChord);
+            tvAuthor = itemView.findViewById(R.id.tvAuthor);
+        }
+    }
+    @NonNull
+    @Override
+    public TrackAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_track, parent, false);
+        return new ViewHolder(view);
+    }
+    @Override
+    public void onBindViewHolder(@NonNull TrackAdapter.ViewHolder viewHolder, int position) {
+        viewHolder.itemView.setTag(trackList.get(position));
+        TrackAdapter.ItemClicked itemClicked = (TrackAdapter.ItemClicked) context;
+        StringBuilder chordValue = new StringBuilder();
+        viewHolder.tvName.setText(String.valueOf(trackList.get(position).getName()));
+        viewHolder.tvAuthor.setText(String.valueOf(trackList.get(position).getAuthor()));
+
+        for (ChordApp chordApp : trackList.get(position).getChordApps()) {
+            chordValue.append(chordApp.getChordName()).append(", ");
+        }
+        viewHolder.tvChord.setText(chordValue);
+        viewHolder.itemView.setOnClickListener(v -> itemClicked.OnItemClicked(trackList.get(position).getId()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return trackList.size();
+    }
+}
