ca819b493a3bd7f90d39bfffe7ce0ee8a9fec486
Michał <mbednaruk12@gmail.com>
2021-05-27 12:43:24 +0200

    change endPoints to get tracks and chords

--- app/src/main/java/pl/bednaruk/httpclient/ApiInterface.java
+++ app/src/main/java/pl/bednaruk/httpclient/ApiInterface.java
@@ -4,20 +4,24 @@ import java.util.List;
 
 import pl.bednaruk.httpclient.models.ChordApp;
 import pl.bednaruk.httpclient.models.Track;
+import pl.bednaruk.httpclient.models.User;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.PATCH;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface ApiInterface {
 
-    @GET("/tracks")
-    Call<List<Track>> getTracks();
+    @GET("/track/all")
+    Call<List<Track>> getTracks(@Header("Authorization") String authorization);
     @GET("/track/{id}")
-    Call<Track> getTrack(@Path("id") int id);
-    @GET("/chords/track/{id}")
-    Call<List<ChordApp>> getChordsByTrackID(@Path("id") int id);
+    Call<Track> getTrack(@Path("id") int id, @Header("Authorization") String authorization);
+    @GET("/chord/track/{id}")
+    Call<List<ChordApp>> getChordsByTrackID(@Path("id") int id, @Header("Authorization") String authorization);
     @POST("/login")
     Call<Void> login(@Body Login login);
    @POST("/user/register")
